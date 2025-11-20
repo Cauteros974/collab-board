c5ec7438de2f857b03408afb298b6fa024d1a47a
Cauteros974 <pusik6666@ukr.net>
2025-11-20 21:01:06 +0200

    Update Layout.tsx

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet, NavLink } from 'react-router-dom';
-import { Facolumns, FaChartPie, FaSignOutAlt } from 'react-icons/fa';
+import { FaColumns, FaChartPie, FaSignOutAlt } from 'react-icons/fa';
 import { useAuthStore } from '../../features/auth/store';
 
 export const Layout = () => {
