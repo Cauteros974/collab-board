2f78433ce5315806fb53f930c8f7081a215bd1dd
Cauteros974 <pusik6666@ukr.net>
2025-11-20 20:29:42 +0200

    Update store.ts

--- src/features/tasks/store.ts
+++ src/features/tasks/store.ts
@@ -3,3 +3,8 @@ import { persist } from 'zustand/middleware';
 import { v4 as uuidv4 } from 'uuid';
 import { toast } from 'sonner';
 import { type User, type Comment, type Task } from "./types";
+
+interface TaskStore {
+    tasks: Task[];
+    addTask: (title: string, status: Status) => void;
+}
