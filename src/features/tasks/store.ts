833416118baa1ad07ed4056c74c2d2264f424c17
Cauteros974 <pusik6666@ukr.net>
2025-11-20 20:30:11 +0200

    Update store.ts

--- src/features/tasks/store.ts
+++ src/features/tasks/store.ts
@@ -7,4 +7,7 @@ import { type User, type Comment, type Task } from "./types";
 interface TaskStore {
     tasks: Task[];
     addTask: (title: string, status: Status) => void;
+    moveTask: (id: string, newStatus: Status) => void;
+    updateTask: (id: string, updates: Partial<Task>) => void;
+    addComment: (taskId: string, comment: Comment) => void;
 }
