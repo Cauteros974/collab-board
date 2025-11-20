0a4bbcfddfaa7359f2671002a009418fad94b0fe
Cauteros974 <pusik6666@ukr.net>
2025-11-20 20:21:41 +0200

    Update store.ts

--- src/features/auth/store.ts
+++ src/features/auth/store.ts
@@ -1,16 +1,24 @@
-import {create } from 'zustand';
+import { create } from 'zustand';
 import { type User } from '../tasks/types';
 
 interface AuthStore {
-    user: User | null;
-    login: (name: string) => void;
-    logout: () => void;
+  user: User | null;
+  login: (name: string) => void;
+  logout: () => void;
 }
 
 export const useAuthStore = create<AuthStore>((set) => ({
-    user: JSON.parse(localStorage.getItem('user') || null),
-    login: (name) => {
-        id:'u1', name,
-        avatar: `https://ui-avatars.com/api/?name=${name}&background=random`
-    }
+  user: JSON.parse(localStorage.getItem('user') || 'null'),
+  login: (name) => {
+    const user = { 
+      id: 'u1', name, 
+      avatar: `https://ui-avatars.com/api/?name=${name}&background=random` 
+    };
+    localStorage.setItem('user', JSON.stringify(user));
+    set({ user });
+  },
+  logout: () => {
+    localStorage.removeItem('user');
+    set({ user: null });
+  },
 }));
