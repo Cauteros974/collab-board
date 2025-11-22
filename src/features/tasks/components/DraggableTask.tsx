8b2bc3411abfaac012a6aa670e4e9f404c6ec32a
Cauteros974 <pusik6666@ukr.net>
2025-11-22 10:04:37 +0200

    Update DraggableTask.tsx

--- src/features/tasks/components/DraggableTask.tsx
+++ src/features/tasks/components/DraggableTask.tsx
@@ -7,3 +7,16 @@ interface Props {
     task: Task;
     onClick: () => void;
 }
+
+export const DraggableTask: React.FC<Props> = ({ task, onClick }) => {
+    const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
+      id: task.id,
+    });
+
+    const style = {
+        transform: CSS.Translate.toString(transform),
+        padding: '12px',
+        backgroundColor: 'var(--color-bg-primary)',
+        marginBottom: '10px',
+        borderRadius: '6px',
+    }
