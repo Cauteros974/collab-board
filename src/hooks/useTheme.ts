e8152a234764d908fae0a772dbae95f615ace251
Cauteros974 <pusik6666@ukr.net>
2025-11-22 10:32:50 +0200

    Update useTheme.ts

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -8,4 +8,10 @@ interface ThemeStore{
 
 export const useThemeStore = create<ThemeStore>((set) => ({
     theme: (localStorage.getItem('theme') as 'light' | 'dark') || 'light',
+    toggleTheme: () =>
+        set((state) => {
+            const newTheme = state.theme === 'light' ? 'dark' : 'light';
+            localStorage.setItem('theme', newTheme);
+            return{ theme: newTheme};
+        })
 }));
