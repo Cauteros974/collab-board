14723d8e2141cd0defe82b8b900db2ff33bdcb82
Cauteros974 <pusik6666@ukr.net>
2025-11-22 10:07:50 +0200

    Update DraggableTask.tsx

--- src/features/tasks/components/DraggableTask.tsx
+++ src/features/tasks/components/DraggableTask.tsx
@@ -33,6 +33,6 @@ export const DraggableTask: React.FC<Props> = ({ task, onClick }) => {
 
     return(
         <div ref = {setNodeRef} style={style} {...listeners} {...attributes} onClick={onClick}>
-            <div style={{ fontWeight: 500}}>{task.title}</div>
+            <div style={{ fontWeight: 500}}>{task.comments}</div>
         </div>
     )
