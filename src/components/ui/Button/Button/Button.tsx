5290e7480723e0710cdf93951ff4173b3869aa31
Cauteros974 <pusik6666@ukr.net>
2025-11-22 10:38:37 +0200

    Update Button.tsx

--- src/components/ui/Button/Button/Button.tsx
+++ src/components/ui/Button/Button/Button.tsx
@@ -3,6 +3,6 @@ import clsx from 'clsx';
 import styles from './Button.module.css';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    variant ? 'primary' | 'secondary' | 'danger';
+    variant?: 'primary' | 'secondary' | 'danger';
 }
 
