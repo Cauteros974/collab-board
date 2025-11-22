59734216508191a464a2fb313a88fdbbf07747fd
Cauteros974 <pusik6666@ukr.net>
2025-11-22 20:10:49 +0200

    Update routes.tsx

--- src/assets/app/routes.tsx
+++ src/assets/app/routes.tsx
@@ -3,18 +3,18 @@ import { Layout } from "../../components/common/Layout";
 import { BoardPage } from "../../features/tasks/pages/BoardPage";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <div style={{padding: 20}}>Welcome to CollabBoard</div> //Layout stub
-        children: [
-            {
-                path: 'board',
-                element: <BoardPage />,
-            },
-            {
-                path: 'chat',
-                element: <div>The chat will be here soon</div>
-            }
-        ]
-    }
-])
+  {
+    path: '/',
+    element: <div style={{ padding: 20 }}>Welcome to CollabBoard</div>, // Layout stub
+    children: [
+      {
+        path: 'board',
+        element: <BoardPage />,
+      },
+      {
+        path: 'chat',
+        element: <div>The chat will be here soon</div>,
+      },
+    ],
+  },
+]);
