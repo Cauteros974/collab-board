c519dfc49cbcd5ddef59aa91b3c6044ca59c4f55
Cauteros974 <pusik6666@ukr.net>
2025-11-23 16:52:50 +0200

    Update LoginPage.tsx

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -6,4 +6,22 @@ export const LoginPage = () => {
     const [name, setName] = useState('');
     const login = useAuthStore((state) => state.login);
     const navigate = useNavigate();
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if(!name.trim()) return;
+
+        //Redirect to the board
+        navigate('/board');
+    };
+
+    return(
+        <div style={{
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            background: 'var(--color-bg-primary)',
+        }}
+    )
 }
