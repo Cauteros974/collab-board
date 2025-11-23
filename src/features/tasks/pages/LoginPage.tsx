bc6cda919cd32be8602902ea200c8c0aa0e92e33
Cauteros974 <pusik6666@ukr.net>
2025-11-23 16:32:00 +0200

    Update LoginPage.tsx

--- src/features/tasks/pages/LoginPage.tsx
+++ src/features/tasks/pages/LoginPage.tsx
@@ -5,4 +5,15 @@ import { useAuthStore } from "../../auth/store";
 export const LoginPage = () => {
     const [name, setName] = useState('');
     const login = useAuthStore((state) => state.login);
+    const navigate = useNavigate();
+
+    const hadleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (!name.trim()) return;
+
+        //Save the user in the store
+        login(name),
+
+        //Redirect to the Dashboard
+    }
 }
