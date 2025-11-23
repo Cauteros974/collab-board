21fae88e8d7adc5ed444c9beb9ad6cc86c28fb00
Cauteros974 <pusik6666@ukr.net>
2025-11-23 16:28:25 +0200

    Update LoginPage.tsx

--- src/features/tasks/pages/LoginPage.tsx
+++ src/features/tasks/pages/LoginPage.tsx
@@ -1,1 +1,8 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../../auth/store";
 
+export const LoginPage = () => {
+    const [name, setName] = useState('');
+    const login = useAuthStore((state) => state.login);
+}
