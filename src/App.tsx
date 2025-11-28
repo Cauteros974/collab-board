eb9181aaaacd207c3023c2eafacbf43dcb1065aa
Cauteros974 <pusik6666@ukr.net>
2025-11-28 19:45:54 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
-import { Toaster } from 'sonner';
+import { Toaster } from 'sonner'; 
 import { router } from './app/routes';
 import { useTheme } from './hooks/useTheme';
 import './theme.css';
 
 const App: React.FC = () => {
+  
   useTheme();
 
   return (
     <React.Fragment>
-      <RouterProvider router={router} />      
+     
+      <RouterProvider router={router} />
+      
       <Toaster position="top-right" richColors closeButton />
     </React.Fragment>
   );
 };
 
 export default App;
+
 import { useEffect } from 'react';
 import { create } from 'zustand';
 
